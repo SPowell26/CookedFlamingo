587c9aee61695c1cff166cf3b5f6bf58a26f746d
Stephan Powell <78470011+SPowell26@users.noreply.github.com>
2023-09-20 02:30:46 -0500

    Add files via upload

---
+++ CookedFlamingo/CookedFlamingo/RootViewController.designer.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace CookedFlamingo
+{
+    [Register("RootViewController")]
+    partial class RootViewController
+    {
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
+
